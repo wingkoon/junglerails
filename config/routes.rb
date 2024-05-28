309dc4a21a9b95a3112a64e0a862917df7e75189
wingkoon <wingkoon@yahoo.com>
2024-05-28 16:26:26 -0700

    user-auth

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
